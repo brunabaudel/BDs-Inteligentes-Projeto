921cd230f2665c93df2197a6d66e128084545cbc
brunabaudel <brubaudel@gmail.com>
2015-06-03 19:26:21 -0300

    Attributes of TelevisionShow, MusicalArtist and Work

--- Projeto/dbpedia_attributes.py
+++ Projeto/dbpedia_attributes.py
@@ -2,6 +2,7 @@ ATTRIBUTES = {
     'owl:Thing':[('label', ['rdfs:label']),
                  ('abstract', ['dbpedia-owl:abstract']),
                  ('website', ['foaf:homepage'])],
+
     'DBpedia:Person':[('birth name', ['dbpprop:birthName']),
                       ('birth date', ['dbpedia-owl:birthDate']),
                       ('birth place', ['dbpedia-owl:birthPlace', 'rdfs:label']),
@@ -13,10 +14,16 @@ ATTRIBUTES = {
                       ('residence', ['dbpedia-owl:residence', 'rdfs:label']), 
                       ('alma mater', ['dbpedia-owl:almaMater', 'rdfs:label']),
                       ('religion', ['dbpedia-owl:religion', 'rdfs:label'])],
+
+    'DBpedia:MusicalArtist':[('genre', ['dbpedia-owl:genre', 'rdfs:label']),
+                             ('instrument', ['dbpprop:instrument'])],
+
     'DBpedia:OfficeHolder':[('party', ['dbpedia-owl:party', 'rdfs:label'])],
+
     'DBpedia:Place':[('area total', ['dbpedia-owl:areaTotal']),
                      ('percentage of area water', ['dbpedia-owl:percentageOfAreaWater']),
                      ('time zone', ['dbpprop:timeZone', 'rdfs:label'])],
+
     'DBpedia:PopulatedPlace':[('capital', ['dbpedia-owl:capital', 'rdfs:label']),
                               ('largest city', ['dbpedia-owl:largestCity', 'rdfs:label']),
                               ('official language', ['dbpedia-owl:officialLanguage', 'rdfs:label']),
@@ -26,11 +33,24 @@ ATTRIBUTES = {
                               ('legislature', ['dbpprop:legislature', 'rdfs:label']),
                               ('population total', ['dbpedia-owl:populationTotal']),
                               ('population density', ['dbpedia-owl:populationDensity'])],
+
     'DBpedia:Country':[('gdp ppp total', ['dbpprop:gdpPpp']),
                        ('gdp ppp per capita', ['dbpprop:gdpPppPerCapita']),
                        ('gdp nominal total', ['dbpprop:gdpNominal']),
                        ('gdo nominal per capita', ['dbpprop:gdpNominalPerCapita']),
                        ('currency', ['dbpedia-owl:currency', 'rdfs:label']),
                        ('date format', ['dbpprop:dateFormat']),
-                       ('drives on the', ['dbpprop:drivesOn'])]
+                       ('drives on the', ['dbpprop:drivesOn'])],
+
+    'DBpedia:Work':[('genre', ['dbpedia-owl:genre', 'rdfs:label'])],
+
+    'DBpedia:TelevisionShow':[('composer', ['dbpedia-owl:composer', 'rdfs:label']),
+                              ('country', ['dbpprop:country']),
+                              ('creator', ['dbpedia-owl:creator', 'rdfs:label']),
+                              ('language', ['dbpprop:language', 'rdfs:label']),
+                              ('num episodes', ['dbpprop:numEpisodes']),
+                              ('num seasons', ['dbpedia-owl:numberOfSeasons']),
+                              ('open theme', ['dbpprop:opentheme']),
+                              ('starring', ['dbpedia-owl:starring', 'rdfs:label'])]
+
 }
